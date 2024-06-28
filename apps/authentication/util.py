82d220c20cf518b03508311ae258d79af9934662
matteoDellaRagione <79637780+matteoDellaRagione@users.noreply.github.com>
2024-06-28 17:44:10 +0200

    Clusterizzazione dei dati 1
    Inizio di clusterizzazione dei dati, fatto host e dnsrecon

--- apps/authentication/util.py
+++ apps/authentication/util.py
@@ -6,6 +6,9 @@ Copyright (c) 2019 - present AppSeed.us
 import os
 import hashlib
 import binascii
+import subprocess
+import re
+import json
 
 # Inspiration -> https://www.vitoshacademy.com/hashing-passwords-in-python/
 
@@ -32,3 +35,97 @@ def verify_pass(provided_password, stored_password):
                                   100000)
     pwdhash = binascii.hexlify(pwdhash).decode('ascii')
     return pwdhash == stored_password
+
+
+def parse_host_output(output):
+    addresses = []
+    mail_servers = []
+
+    # Espressioni regolari per trovare gli indirizzi IP e i server mail
+    address_pattern = re.compile(r'has address ([\d\.]+)')
+    mail_pattern = re.compile(r'mail is handled by \d+ ([\w\.\-]+)')
+
+    # Parso l'output per trovare le corrispondenze
+    for line in output.splitlines():
+        address_match = address_pattern.search(line)
+        mail_match = mail_pattern.search(line)
+        if address_match:
+            addresses.append(address_match.group(1))
+        if mail_match:
+            mail_servers.append(mail_match.group(1))
+
+    # Creare un dizionario con i risultati
+    host_data = {
+        "indirizzi": addresses,
+        "server mail": mail_servers
+    }
+    
+    return host_data
+
+def host(domain):
+     # Esegui il comando host
+    result = subprocess.run(['host', domain], capture_output=True, text=True)
+    return parse_host_output(result.stdout)  
+
+def whois_to_json(domain):
+    # Esegui il comando whois
+    result = subprocess.run(['whois', domain], capture_output=True, text=True)
+    whois_output = result.stdout
+    
+    # Parso l'output di whois
+    whois_data = {}
+    for line in whois_output.splitlines():
+        match = re.match(r"^(.*?):\s*(.*)$", line)
+        if match:
+            key, value = match.groups()
+            whois_data[key.strip()] = value.strip()
+    
+    # Converti in JSON
+    return json.dumps(whois_data, indent=4)
+
+def dnsrecon(domain):
+    # Esegui il comando dnsrecon per il dominio specificato
+    result = subprocess.run(['dnsrecon', '-d', domain], capture_output=True, text=True)
+    output = result.stdout
+    
+    # Analizza l'output
+    mail_servers = []
+    addresses = []
+    conf = []
+    
+    for line in output.splitlines():
+        if 'MX' in line:
+            parts = line.split()
+            mail_servers.append(parts[-1])
+        elif f'A {domain}' in line:
+            parts = line.split()
+            addresses.append(parts[-1])
+        elif f'TXT {domain}' in line:
+            conf.append(line.split(f'{domain} ')[-1])
+    
+    # Crea il dizionario JSON
+    data = {
+        'server mail': mail_servers,
+        'indirizzi': addresses,
+        'conf': conf
+    }
+    
+    return data
+
+def merge_json(json1, json2):
+    merged = {}
+
+    # Unisci indirizzi
+    indirizzi_set = set(json1.get('indirizzi', [])) | set(json2.get('indirizzi', []))
+    merged['indirizzi'] = list(indirizzi_set)
+
+    # Unisci server mail
+    mail_set = set(json1.get('server mail', [])) | set(json2.get('server mail', []))
+    merged['server mail'] = list(mail_set)
+
+    # Unisci conf se esiste(da modificare visto che potrebbe esserci o no conf solo nel json2)
+    if 'conf' in json1 or 'conf' in json2:
+        conf_set = set(json1.get('conf', [])) | set(json2.get('conf', []))
+        merged['conf'] = list(conf_set)
+    
+    return merged
--- apps/home/routes.py
+++ apps/home/routes.py
@@ -2,67 +2,23 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-import subprocess
-import re
-import json
+
 from apps.home import blueprint
-from flask import render_template, request
+from flask import render_template, request,jsonify
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+from apps.authentication.util import *
 
-def whois_to_json(domain):
-    # Esegui il comando whois
-    result = subprocess.run(['whois', domain], capture_output=True, text=True)
-    whois_output = result.stdout
-    
-    # Parso l'output di whois
-    whois_data = {}
-    for line in whois_output.splitlines():
-        match = re.match(r"^(.*?):\s*(.*)$", line)
-        if match:
-            key, value = match.groups()
-            whois_data[key.strip()] = value.strip()
-    
-    # Converti in JSON
-    return json.dumps(whois_data, indent=4)
 
-def parse_host_output(output):
-    addresses = []
-    mail_servers = []
-
-    # Espressioni regolari per trovare gli indirizzi IP e i server mail
-    address_pattern = re.compile(r'has address ([\d\.]+)')
-    mail_pattern = re.compile(r'mail is handled by \d+ ([\w\.\-]+)')
-
-    # Parso l'output per trovare le corrispondenze
-    for line in output.splitlines():
-        address_match = address_pattern.search(line)
-        mail_match = mail_pattern.search(line)
-        if address_match:
-            addresses.append(address_match.group(1))
-        if mail_match:
-            mail_servers.append(mail_match.group(1))
-
-    # Creare un dizionario con i risultati
-    host_data = {
-        "indirizzi": addresses,
-        "server mail": mail_servers
-    }
-    
-    return json.dumps(host_data, indent=4)
-
-def host(domain):
-    # Esegui il comando whois
-    result = subprocess.run(['host', domain], capture_output=True, text=True)
-    return result.stdout    
+  
 
 @blueprint.route('/index')
 @login_required
 def index():
-    domain = "nttdata.com"
+    domain = "conad.it"
     whois_json = whois_to_json(domain)
-    host_json= parse_host_output(host(domain))
-    return render_template('home/index.html', segment='index',whois_json=whois_json,host_json=host_json)
+    combined_json = merge_json(dnsrecon(domain), host(domain))
+    return render_template('home/index.html', segment='index',whois_json=whois_json,combined_json=combined_json)
 
 
 @blueprint.route('/<template>')
